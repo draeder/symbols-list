3cf789e85e57b262a2868550e08005469c209bde
draeder <draeder@gmail.com>
2020-09-25 20:13:36 -0500

    updates

--- Symbols.js
+++ Symbols.js
@@ -18,7 +18,7 @@ Symbols.prototype.get = function (event, data) {
         url: 'https://api.iextrading.com/1.0/ref-data/symbols'
     }, (error, response, body) => {
         if(error) return console.log(error)
-        //console.log(response)
+
         body = JSON.parse(body)
         let type
         if(event == "type" && body) {
@@ -45,28 +45,54 @@ Symbols.prototype.get = function (event, data) {
         let results = []
         if(event == "random" && body) {
             let type = data.type
+            let n = data.size
+            if(!type){type="all"}
   
             if(type && type == "stock"){
                 type="N/A"
+                body.forEach(element => {
+                    if(type && element.type==type){
+                        results.push(element)
+                    }
+                })
             }
             if(type && type == "crypto"){ 
-                type="crypto" 
+                type="crypto"
+                body.forEach(element => {
+                    if(type && element.type==type){
+                        results.push(element)
+                    }
+                })
             }
-            body.forEach(element => {
-                if(type && element.type==type){
+            if(type && type == "all" || !type){
+                body.forEach(element => {
                     results.push(element)
-                } else {
-                    results.push(element)
-                }
-            })
-            const n = data.size
+                })
+            }
+            if(typeof(data)==="number"){
+                n = data
+            }
             const sample = results
                 .map(x => ({ x, r: Math.random() }))
                 .sort((a, b) => a.r - b.r)
                 .map(a => a.x)
-                .slice(0, n);
+                .slice(0, n)
+
             symbols.emit("random", sample)
 
+        }
+
+        if(event == "find" && body) {
+            let results = []
+            if(typeof(data)==="object"){
+                data.forEach(item => {
+                    results.push(body.find(element => element.symbol == item))
+                })
+                console.log(results)
+            } else if (typeof(data)==="string"){
+                const found = body.find(element => element.symbol == data)
+                console.log(found)
+            }
         }
 
     })
--- server.js
+++ server.js
@@ -11,33 +11,16 @@ symbols.on("random", data => {
     console.log(data)
 })
 
+symbols.on("find", data => {
+    console.log(data)
+})
+
 //symbols.get("all") // return all symbols
-symbols.get("find", "TSLA") // find a single symbol
-symbols.get("find", ["TSLA", "MSFT", "BTC"]) // find a list of symbols
+//symbols.get("find", "TSLA") // find a single symbol
+//symbols.get("find", ["TSLA", "MSFT", "AAPL"]) // find a list of symbols
 //symbols.get("type", "stock") // return all stock symbols
 //symbols.get("type", "crypto") // return all crypto symbols
 //symbols.get("random", {type: "crypto", size: 2}) // return random number of symbols for the given type. Default type is "all"
-symbols.get("random", {type: "crypto", size: 2}) 
-symbols.get("random", {size: 3}) 
-
-/* STOCK example
-{
-    symbol: "ETFC",
-    name: "E*TRADE FINANCIAL CORP",
-    date: "2020-09-25",
-    isEnabled: true,
-    type: "N/A",
-    iexId: "2273"
-  }
-*/
-
-/* CRYPTO example
-{
-    symbol: "BNBUSDT",
-    name: "Binance Coin USD",
-    date: "2020-09-25",
-    isEnabled: true,
-    type: "crypto",
-    iexId: 10000003
-}
-*/
+//symbols.get("random", {type: "stock", size: 2}) 
+//symbols.get("random", {size: 2}) 
+//symbols.get("random", 3) 
